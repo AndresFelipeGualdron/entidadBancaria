3b08c160dffebf79915a5cc048bdde13ef6bc859
GYEISSON <yeison7f@gmail.com>
2021-02-21 14:54:29 -0500

    otros diccionarios

--- models/persistence.php
+++ models/persistence.php
@@ -7,28 +7,32 @@ class Persistence{
     //MAP Usuarios
     public $mapUsuarios;
     //MAP Cuenta
-    public 
+    public $mapCuentas;
+    //MAP Empleados
+    public $mapEmpleados;
 
     
     function __construct(){
-        $this->$mapUsuarios = new \Ds\Map();;
+        $this->$mapUsuarios = new \Ds\Map();
+        $this->$mapCuentas = new \Ds\Map();
+        $this->$mapEmpleados = new \Ds\Map();
     }
 
 
-    function add($usuario){
+    function addUser($usuario){
         $var=1;
         $this->$mapUsuarios->put($usuario->getIdUsuario,$usuario);
         return($var);
     }
 
-    function modify($idUser,$username){
+    function modifyUser($idUser,$username){
         $var=1;
         $xUser =  $this->$mapUsuarios->remove($idUser);
         $this->lista->put($xUser);
         return($var);
     }
 
-    function delete($idUser){
+    function deleteUser($idUser){
         $var=1;
         $this->$mapUsuarios->remove($idUser);
         return($var);
