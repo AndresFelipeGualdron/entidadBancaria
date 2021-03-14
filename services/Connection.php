319a0161877ef5a3f0196dbf8fdf16f8e1c3bed1
agualdr <gualdronsito@hotmail.com>
2021-03-14 17:14:24 -0500

    merge

--- services/Connection.php
+++ services/Connection.php
@@ -1,22 +1,6 @@
 <?php
 
 class Connection {
-    
-<<<<<<< HEAD
-    public static $host="db-bank.coyu3youhn6x.us-east-1.rds.amazonaws.com";
-    public static $port=3306;
-    public static $socket="";
-    public static $user="masterUsername";
-    public static $password="gualdrongualdron";
-=======
-    public  $host="db-bank.coyu3youhn6x.us-east-1.rds.amazonaws.com";
-    public  $port=3306;
-    public  $socket="";
-    public  $user="masterUsername";
-    public  $password="gualdrongualdron";
->>>>>>> ed26ab9f6517aaa091b00d5ab6ebde9402406719
-    
-    
     public  function connect3(){
         $mbd = new PDO("mysql:dbname=dbbank;host=db-bank.coyu3youhn6x.us-east-1.rds.amazonaws.com", "masterUsername", "gualdrongualdron",
                 array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
